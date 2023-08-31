ec4cbb4f23d3c352e965eff7c7cf23fb978fc62b
OlukaGibson <gibsonoluka7@gmail.com>
2023-08-31 11:11:11 +0300

    Initial commit

---
+++ apiapp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
+# class SensorData(models.Model):
+#     temperature = models.FloatField()
+#     humidity = models.FloatField()
---
+++ apiapp/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('update_data/', views.update_data, name='update_data'),
+]
